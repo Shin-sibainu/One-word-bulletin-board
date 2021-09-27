c5ddfd5637fa10c68fb9b6936f611a4538748cf2
Shin-sibainu <shincode0712@gmail.com>
2021-09-27 11:40:46 +0900

    diplay data on html. next is data varidation

--- index.php
+++ index.php
@@ -12,8 +12,9 @@ $file_handle = null;
 $split_data = null;
 $message = array();
 $message_array = array();
+$success_message = null;
 
-//ページ表示だけなのか、それとも書き込みなのかの判定。
+//もし、書き込まれたら
 if (!empty($_POST["btn-submit"])) {
 //ファイルかURLをオープンする。（書き込みモード）
     if ($file_handle = fopen(FILENAME, "a")) {
@@ -29,29 +30,39 @@ if (!empty($_POST["btn-submit"])) {
 
         //安全に閉じる。
         fclose($file_handle);
+
+        $success_message = "メッセージを書き込みました";
     }
 }
 
-//送信ボタンを押さずともこのif文はファイルさえ存在すれば通る。
 //データが書かれたファイルを読み込む
 if ($file_handle = fopen(FILENAME, "r")) {
     //ファイルに書いてるデータを１行ずつ取得する。できたらtrueを返す。
     while ($data = fgets($file_handle)) {
-        echo $data . "<br>";
         $split_data = preg_split('/\'/', $data);
 
         $message = array(
             "view_name" => $split_data[1], //表示名
             "message" => $split_data[3], //一言メッセージ
-            //"post_date" => $split_data[5], //投稿日時
+            "post_date" => $split_data[5], //投稿日時
         );
+        array_unshift($message_array, $message);
     }
     //ファイルを閉じる
     fclose($file_handle);
 }
 ?>
 
-<!-- action属性は情報を処理するプログラムのURI（今回は空でこのファイルのURIをデフォルトで指定している。だから上のphpが実行されている。） -->
+
+
+<h1>ひと言掲示板</h1>
+
+<!-- メッセージ成功用 -->
+<?php if (!empty($success_message)): ?>
+    <p><?php echo $success_message ?></p>
+<?php endif;?>
+
+<!-- form送信用 -->
 <form method="post" action="">
 	<div>
 		<label for="view-name">表示名</label>
@@ -63,3 +74,26 @@ if ($file_handle = fopen(FILENAME, "r")) {
 	</div>
 	<input type="submit" value="書き込む" name="btn-submit">
 </form>
+
+
+
+<hr>
+
+
+<!-- ひと言表示用 -->
+<section>
+    <?php if (!empty($message_array)): ?>
+        <!-- HTMLに表示するデータを配列から１つずつ取り出し、表示する。 -->
+        <?php foreach ($message_array as $value): ?>
+            <article>
+                <div class="info">
+                    <h2><?php echo $value["view_name"] ?></h2>
+                    <time><?php echo date("Y年m月d日 H:i", strtotime($value["post_date"])) ?></time>
+                </div>
+                <p><?php echo $value["message"] ?></p>
+            </article>
+        <?php endforeach;?>
+        <?php endif;?>
+</section>
+
+
